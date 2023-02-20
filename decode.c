292dc78065e1fdc1759b136a64f06bbbfebce9bc
ChinnyChilla <michaelzheng04@gmail.com>
2023-02-20 18:09:27 -0500

    potentially final

--- decode.c
+++ decode.c
@@ -12,15 +12,40 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Please invoke as %s <key>\n", argv[0]);
 		return 1; // Indicate something went wrong
 	}
-	char *key=argv[1];
+	char *key = argv[1];
 	int nextChar = getchar();
+	int whatToShiftBy = 0;
+	int currentIndex = 1;
+	while (*key != '\0')
+	{
+		char currentChar = *key;
+		whatToShiftBy += ((int)currentChar) * currentIndex;
+		currentIndex++;
+		key++;
+	}
+	currentIndex = 0;
+	int maxIndex = 93;
+	int offset = 33;
 	while (nextChar != EOF)
 	{
+		if ((int)nextChar >= 33 && (int)nextChar <= 126) {
+
+			int currentOffset = ((int)nextChar) - whatToShiftBy - currentIndex;
+			while (currentOffset < offset) {
+				currentOffset += maxIndex;
+			}
+			// printf("This is the number %d\n", currentOffset);
+			printf("%c", (char) currentOffset);
+		} else {
+			printf("%c", nextChar);
+		}
+		currentIndex++;
 		/* TODO:
-			Using the key, write an encoded character to standard out
-			For example, if you encode nextChar and save it in variable enCoded,
-			You can use: putchar(enCoded);
+			Using the key, write an decoded character to standard out
+			For example, if you decode nextChar and save it in variable deCoded,
+			You can use: putchar(deCoded);
 		*/
+		currentIndex++;
 		nextChar = getchar();
 	}
 	return 0; // Indicate nothing went wrong
--- encode.c
+++ encode.c
@@ -14,13 +14,32 @@ int main(int argc, char **argv)
 	}
 	char *key=argv[1];
 	int nextChar = getchar();
+	int whatToShiftBy = 0;
+	int currentIndex = 1;
+	while (*key != '\0') {
+		char currentChar = *key;
+		whatToShiftBy += ((int) currentChar) * currentIndex;
+		currentIndex++;
+		key++;
+	}
+	currentIndex = 0;
+	int maxIndex = 93;
+	int offset = 33;
 	while (nextChar != EOF)
 	{
+		if ((int) nextChar >=33 && (int) nextChar <=126) {
+			int test = ((int)nextChar) - offset + whatToShiftBy + currentIndex;
+			printf("%c", (char)(test % maxIndex + offset));
+		} else {
+			printf("%c", nextChar);
+		}
+		currentIndex++;
 		/* TODO:
 			Using the key, write an decoded character to standard out
 			For example, if you decode nextChar and save it in variable deCoded,
 			You can use: putchar(deCoded);
 		*/
+	currentIndex++;
 		nextChar = getchar();
 	}
 	return 0; // Indicate nothing went wrong
